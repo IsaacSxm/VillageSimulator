6f9cce92f2a2bbdf0c984f20c7dd8ee1b7180d27
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-13 21:05:31 +0200

    Continue Rainforest class

--- src/classes/environment/RainForest.java
+++ src/classes/environment/RainForest.java
@@ -5,4 +5,10 @@ import interfaces.EnvironmentCondition;
 public class RainForest implements EnvironmentCondition {
     //    Todo: Virag
 
+    protected final static int MINIMUM_WARRIOR_SPAWN = 0;
+    protected final static int MAXIMUM_WARRIOR_SPAWN = 0;
+    protected final static int MINIMUM_POISON_SPAWN = 0;
+    protected final static int MAXIMUM_POISON_SPAWN = 0;
+    protected final static int MINIMUM_GOOD_SPAWN = 0;
+    protected final static int MAXIMUM_GOOD_SPAWN = 0;
 }
