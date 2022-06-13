edf4bdc9402e0d35c30a5d18112dfe0a14662bf7
IsaacSxm <rhymefirst1@gmail.com>
2022-06-13 19:31:11 +0200

    Implementing village class

--- src/classes/village/Village.java
+++ src/classes/village/Village.java
@@ -1,4 +1,30 @@
 package classes.village;
 
+import classes.roles.Roles;
+import interfaces.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Village {
+
+    protected List<Roles> population;
+    protected List<Food> food;
+
+    public Village() {
+        this.population = new ArrayList<>();
+        this.food = new ArrayList<>();
+    }
+
+    public int getPopulation() {
+        return population.size();
+    }
+
+    public void setPopulation(List<Roles> population) {
+        this.population = population;
+    }
+
+    public void setPopulation(List<Roles> population) {
+        this.population = population;
+    }
 }
--- src/classes/village/VillagerPerson.java
+++ src/classes/village/VillagerPerson.java
@@ -1,4 +1,22 @@
 package classes.village;
 
+import interfaces.Food;
+
 public class VillagerPerson implements Villager{
+    private String role;
+    private int health;
+    private int age;
+    private String gender;
+    private boolean isAlive;
+    private Food food;
+
+    public VillagerPerson(VillagerBuilder villagerBuilder) {
+        this.health = villagerBuilder.getHealth();
+        this.gender = villagerBuilder.getGender();
+        this.age = villagerBuilder.getAge();
+        this.gender = villagerBuilder.getGender();
+//        this.isAlive = villagerBuilder.isAlive();
+    }
+
+
 }
