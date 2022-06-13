8c93985357f8933b31d97f11b5f3723b78c68983
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-13 19:46:48 +0200

    Start building up the environment

--- src/classes/environment/Environment.java
+++ src/classes/environment/Environment.java
@@ -1,5 +1,19 @@
 package classes.environment;
 
+import classes.village.Village;
+import interfaces.EnvironmentCondition;
+
 public class Environment {
     //    Todo: Virag
+
+    protected EnvironmentCondition environmentCondition;
+    protected Village village;
+    protected int duration = 0;
+    protected int numberOfWarriors = 0;
+    protected int numberOfFarmers = 0;
+
+    public Environment(EnvironmentCondition condition) {
+        this.environmentCondition = condition;
+        this.village = new Village();
+    }
 }
