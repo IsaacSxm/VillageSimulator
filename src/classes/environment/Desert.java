b278cda9f257e08878acc9f58364937d8bfebc9b
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-13 20:56:34 +0200

    Start building up the Desert class

--- src/classes/environment/Desert.java
+++ src/classes/environment/Desert.java
@@ -4,4 +4,12 @@ import interfaces.EnvironmentCondition;
 
 public class Desert implements EnvironmentCondition {
     //    Todo: Virag
+
+    protected final static int MINIMUM_WARRIOR_SPAWN = 0;
+    protected final static int MAXIMUM_WARRIOR_SPAWN = 0;
+    protected final static int MINIMUM_POISON_SPAWN = 0;
+    protected final static int MAXIMUM_POISON_SPAWN = 0;
+    protected final static int MINIMUM_GOOD_SPAWN = 0;
+    protected final static int MAXIMUM_GOOD_SPAWN = 0;
+
 }
