00e894dc40ccee67a7dc635378bcceae2c28213e
Jonathan Mohamed <jonathanmohamed34@gmail.com>
2022-06-14 23:10:46 +0200

    Create a villager with a role

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -8,15 +8,15 @@ import classes.roles.Farmer;
 import classes.roles.Roles;
 import classes.roles.Warrior;
 import classes.village.VillagerBuilder;
-
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     private Environment environment;
     private Scanner userInput = new Scanner(System.in);
     private int startCommand;
+    private int villagerCommand;
     private VillagerBuilder villagerBuilder = new VillagerBuilder();
+    private String newVillagerCommand;
     private int numberOfDays;
 
 
@@ -37,7 +37,34 @@ public class Main {
         this.userInput = scanner;
     }
     public void creatAVillager() {
-
+        System.out.println("You have successfully created a " + this.environment.getEnvironmentCondition().getConditionType());
+        System.out.println("Press 1 to create a chief");
+        System.out.println("Press 2 to create a farmer");
+        System.out.println("Press 3 to create a warrior");
+        villagerCommand = userInput.nextInt();
+        switch (villagerCommand) {
+            case 1 -> {
+                Roles role1 = new Chief(this.villagerBuilder.build());
+                this.environment.getVillage().addVillager(role1);
+                System.out.println(role1.getTitle() + " villager created");
+            }
+            case 2 -> {
+                Roles role2 = new Farmer(this.villagerBuilder.build());
+                this.environment.getVillage().addVillager(role2);
+                System.out.println(role2.getTitle() + " villager created");
+            }
+            case 3 -> {
+                Roles role3 = new Warrior(this.villagerBuilder.build());
+                this.environment.getVillage().addVillager(role3);
+                System.out.println(role3.getTitle() + " villager created");
+            }
+        }
+        System.out.println("Press N to create another Villager or X to start simulation");
+        this.newVillagerCommand = userInput.next();
+        switch (this.newVillagerCommand) {
+            case "N" -> this.creatAVillager();
+            case "X" -> this.simulate();
+        }
     }
     private void simulate() {
 
