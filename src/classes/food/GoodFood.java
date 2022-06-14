b30af64d0030eae1271696eeb456148e6474450e
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-14 23:55:05 +0200

    Good Food: comments, organizing part 2

--- src/classes/food/GoodFood.java
+++ src/classes/food/GoodFood.java
@@ -1,7 +1,7 @@
 package classes.food;
 
 import interfaces.Food;
-//ToDo: Virag
+
 public class GoodFood implements Food {
     protected static final int MINIMUM_HEALTH = 5;
     protected static final int MAXIMUM_HEALTH = 60;
