36bef1e27caad486dcaaf04f86132e11ebf35706
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-14 11:41:55 +0200

    Continue Deser class - add the min and max numbers

--- src/classes/environment/Desert.java
+++ src/classes/environment/Desert.java
@@ -5,11 +5,11 @@ import interfaces.EnvironmentCondition;
 public class Desert implements EnvironmentCondition {
     //    Todo: Virag
 
-    protected final static int MINIMUM_WARRIOR_SPAWN = 0;
-    protected final static int MAXIMUM_WARRIOR_SPAWN = 0;
-    protected final static int MINIMUM_POISON_SPAWN = 0;
-    protected final static int MAXIMUM_POISON_SPAWN = 0;
-    protected final static int MINIMUM_GOOD_SPAWN = 0;
-    protected final static int MAXIMUM_GOOD_SPAWN = 0;
+    protected final static int MINIMUM_WARRIOR_SPAWN = 1;
+    protected final static int MAXIMUM_WARRIOR_SPAWN = 5;
+    protected final static int MINIMUM_POISON_SPAWN = 1;
+    protected final static int MAXIMUM_POISON_SPAWN = 2;
+    protected final static int MINIMUM_GOOD_SPAWN = 1;
+    protected final static int MAXIMUM_GOOD_SPAWN = 3;
 
 }
