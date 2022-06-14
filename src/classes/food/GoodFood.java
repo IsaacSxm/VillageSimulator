3eaa6c45e9e3cb743f86964dcc62d952e535f90a
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-14 23:54:50 +0200

    Good Food: comments, organizing

--- src/classes/food/GoodFood.java
+++ src/classes/food/GoodFood.java
@@ -7,12 +7,14 @@ public class GoodFood implements Food {
     protected static final int MAXIMUM_HEALTH = 60;
 
     @Override
+    //  Get the type of the food
     public String getFoodType() {
 
         return "Good";
     }
 
     @Override
+    //  Get the impact of the good food
     public int getImpact() {
 
         return (int) ((Math.random() * (MAXIMUM_HEALTH - MINIMUM_HEALTH)) + MINIMUM_HEALTH);
