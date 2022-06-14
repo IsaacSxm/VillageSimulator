ac470196dea80bfc4ff23e61a0fe57827efee253
Jonathan Mohamed <jonathanmohamed34@gmail.com>
2022-06-14 23:01:04 +0200

    Create a villager

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -13,12 +13,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private Environment environment;
     private Scanner userInput = new Scanner(System.in);
+    private int startCommand;
     private VillagerBuilder villagerBuilder = new VillagerBuilder();
+    private int numberOfDays;
 
 
     public void startMessage() {
-
+        System.out.println("This is a Villager simulation");
+        System.out.print("Press 1 to select a Desert environment or 2 to select a Rain forest environment");
+        startCommand = userInput.nextInt();
+        switch (startCommand) {
+            case 1 -> this.environment = new Environment(new Desert());
+            case 2 -> this.environment = new Environment(new RainForest());
+        }
+        System.out.print("Enter the number of days you want your simulation to run for");
+        this.numberOfDays = userInput.nextInt();
+        this.creatAVillager();
     }
 
     public void setUserInput(Scanner scanner) {
