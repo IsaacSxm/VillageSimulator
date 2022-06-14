49f22fdbd9b3fba3aa96e046268692431a735bc3
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-14 20:24:00 +0200

    Desert class functions - comments

--- src/classes/environment/Desert.java
+++ src/classes/environment/Desert.java
@@ -4,7 +4,6 @@ import interfaces.EnvironmentCondition;
 
 public class Desert implements EnvironmentCondition {
     //    Todo: Virag
-
     protected final static int MINIMUM_WARRIOR_SPAWN = 1;
     protected final static int MAXIMUM_WARRIOR_SPAWN = 5;
     protected final static int MINIMUM_POISON_SPAWN = 1;
