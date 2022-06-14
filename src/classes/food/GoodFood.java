00fe372dc4a140c72e853dc47b81f25e8f32b130
Jonathan Mohamed <jonathanmohamed34@gmail.com>
2022-06-14 20:59:31 +0200

    Implemented interfaces into food

--- src/classes/food/GoodFood.java
+++ src/classes/food/GoodFood.java
@@ -3,4 +3,13 @@ package classes.food;
 import interfaces.Food;
 
 public class GoodFood implements Food {
+    @Override
+    public String getFoodType() {
+        return null;
+    }
+
+    @Override
+    public int getImpact() {
+        return 0;
+    }
 }
--- src/classes/food/PoisonFood.java
+++ src/classes/food/PoisonFood.java
@@ -3,4 +3,13 @@ package classes.food;
 import interfaces.Food;
 
 public class PoisonFood implements Food {
+    @Override
+    public String getFoodType() {
+        return null;
+    }
+
+    @Override
+    public int getImpact() {
+        return 0;
+    }
 }
