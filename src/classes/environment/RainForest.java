4e8843595152cc2ab49b79a705d2ed75c4abf469
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-14 11:39:26 +0200

    Continue Rainforest class - fix the numbers

--- src/classes/environment/RainForest.java
+++ src/classes/environment/RainForest.java
@@ -7,8 +7,8 @@ public class RainForest implements EnvironmentCondition {
 
     protected final static int MINIMUM_WARRIOR_SPAWN = 1;
     protected final static int MAXIMUM_WARRIOR_SPAWN = 5;
-    protected final static int MINIMUM_POISON_SPAWN = 1;
-    protected final static int MAXIMUM_POISON_SPAWN = 2;
+    protected final static int MINIMUM_POISON_SPAWN = 2;
+    protected final static int MAXIMUM_POISON_SPAWN = 8;
     protected final static int MINIMUM_GOOD_SPAWN = 1;
-    protected final static int MAXIMUM_GOOD_SPAWN = 3;
+    protected final static int MAXIMUM_GOOD_SPAWN = 5;
 }
