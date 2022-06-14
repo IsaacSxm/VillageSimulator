52ad33bfa0074acb877b8f096c7f14f33499e1f2
ViragSzabo <szabo.viragnak@gmail.com>
2022-06-14 21:34:32 +0200

    Desert: organizing

--- src/classes/environment/Desert.java
+++ src/classes/environment/Desert.java
@@ -12,25 +12,21 @@ public class Desert implements EnvironmentCondition {
 
     //  Get Condition Type function
     public String getConditionType(){
-        //  get the type of the environment
         return "Desert";
     }
 
     //  Warrior Spawn function
     public int spawnWarriors(){
-        //  counting
         return (int) ((Math.random() * (MAXIMUM_WARRIOR_SPAWN - MINIMUM_WARRIOR_SPAWN)) + MINIMUM_WARRIOR_SPAWN);
     }
 
     //  Poison Spawn function
     public int spawnPoisonFood(){
-        //  counting
         return (int) ((Math.random() * (MAXIMUM_POISON_SPAWN - MINIMUM_POISON_SPAWN)) + MINIMUM_POISON_SPAWN);
     }
 
     //  Good Spawn function
     public int spawnGoodFood(){
-        //  counting
         return  (int) ((Math.random() * (MAXIMUM_GOOD_SPAWN - MINIMUM_GOOD_SPAWN)) + MINIMUM_GOOD_SPAWN);
     }
 }
