815200c4ac9aa7797219bc7d6f499ee0cb91969b
Jonathan Mohamed <jonathanmohamed34@gmail.com>
2022-06-14 22:58:24 +0200

    Setting up main to run the simulator

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -1,5 +1,33 @@
 package classes;
 
-public class Main {
+import classes.environment.Desert;
+import classes.environment.Environment;
+import classes.environment.RainForest;
+import classes.roles.Chief;
+import classes.roles.Farmer;
+import classes.roles.Roles;
+import classes.roles.Warrior;
+import classes.village.VillagerBuilder;
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private Scanner userInput = new Scanner(System.in);
+    private VillagerBuilder villagerBuilder = new VillagerBuilder();
+
+
+    public void startMessage() {
+
+    }
+
+    public void setUserInput(Scanner scanner) {
+        this.userInput = scanner;
+    }
+    public void creatAVillager() {
+
+    }
+    private void simulate() {
+
+    }
 }
